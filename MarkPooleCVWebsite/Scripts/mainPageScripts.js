d9193c819cc3d1517a70dacf90f9835bd6b3b476
MarkyPoole <markypoole@gmail.com>
2015-12-17 15:54:42 +0000

    Initial Commit

---
+++ MarkPooleCVWebsite/Scripts/mainPageScripts.js
@@ -0,0 +1,66 @@
+function resizeProfile() {
+	$(window).resize(function () {
+                    var bodyheight = $(window).height();
+                    $("#profile").css({
+                        'top': bodyheight + 'px'
+                    });
+                }).resize();
+};
+
+function resizeMainBody() {
+	$(window).resize(function () {
+                    var bodyHeight = $(window).height() + $('#profile').height();
+                    $('#mainBody').css({
+                        'top': bodyHeight + 'px'
+                    });
+                }).resize();
+};
+
+function resizeMainBodyWCLInfo() {
+	$(window).resize(function () {
+                    var totalHeight = $('#WLCInfo').height();
+                    $('#WestLothianCollege').css({
+                        'height': totalHeight + 'px'
+                    });
+                }).resize();
+};
+
+function sidebarWrapper() {
+ 	var navBarWidth = $("#sidebar-wrapper").width();
+                $("#page-wrap").css({
+                    'left': navBarWidth + 'px'
+                })
+
+                $(window).resize(function () {
+                    $("#page-wrap").css({
+                        'left': navBarWidth + 'px'
+                    })
+                }).resize();
+};
+
+function resizeLineBreak () {
+	$(window).resize(function () {
+                    var totalHeight = $('#WLCInfo').height();
+                    $('#lineBreakAfterWLC').css({
+                        'height': totalHeight + 40 + 'px'
+                    });
+                }).resize();
+};
+
+function resizeExperience() {
+	$(window).resize(function () {
+                    var bodyHeight = $(window).height() + $('#profile').height() + $('#mainBody').height();
+                    $('#experience').css({
+                        'top': bodyHeight + 'px'
+                    });
+                }).resize();
+};
+
+function smoothScroll() {
+ 	$('a').click(function () {
+                $('html, body').animate({
+                    scrollTop: $($.attr(this, 'href')).offset().top
+                }, 500);
+                return false;
+            });
+};
